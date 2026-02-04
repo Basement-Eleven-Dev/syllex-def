c89aac4ae156d725ead5a09532a98c76b9c17f69
Action merge <amatipierluigi@gmail.com>
2026-02-04 16:26:56 +0100

    added user to lambda context

--- backend/src/_helpers/_lambda/lambdaProxyResponse.ts
+++ backend/src/_helpers/_lambda/lambdaProxyResponse.ts
@@ -1,11 +1,23 @@
 import middy from "@middy/core";
 import httpResponseSerializer from "@middy/http-response-serializer";
-import { Handler } from "aws-lambda";
+import { Context, Handler } from "aws-lambda";
 import cors from '@middy/http-cors';
 import httpErrorHandler from "@middy/http-error-handler";
+import { getCurrentUser, User } from "../getAuthCognitoUser";
 
-export const lambdaRequest = (handler: Handler) => {
+declare module 'aws-lambda' {
+  interface Context {
+    user?: User | null;
+  }
+}
+
+export const lambdaRequest = (handler: any) => {
   return middy(handler)
+    .use({
+      before: async (request) => {
+        request.context.user = await getCurrentUser(request.event);
+      }
+    })
     .use(
       httpResponseSerializer({
         serializers: [
--- backend/src/functions-refactoring/status.ts
+++ backend/src/functions-refactoring/status.ts
@@ -1,12 +1,10 @@
 import { APIGatewayProxyEvent, Context, Handler } from "aws-lambda";
-import middy from '@middy/core';
-import cors from '@middy/http-cors';
-import httpResponseSerializer from "@middy/http-response-serializer";
 import { lambdaRequest } from "../_helpers/_lambda/lambdaProxyResponse";
 import createError from 'http-errors'
 
 
 const getStatus = async (event: APIGatewayProxyEvent, context: Context) => {
+    console.log(context.user);
     const status: boolean = Math.random() < 0.5;
     if (!status) throw new createError.ServiceUnavailable('Offline')
     return {
