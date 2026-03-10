e970cdd65ca8dbf20b1fca2b72afbde51f5a128a
Action merge <amatipierluigi@gmail.com>
2026-03-10 10:09:55 +0100

    test subject-id

--- backend/src/_helpers/lambdaProxyResponse.ts
+++ backend/src/_helpers/lambdaProxyResponse.ts
@@ -19,11 +19,13 @@ export const lambdaRequest = (handler: any) => {
   return middy(handler)
     .use({
       before: async (request) => {
-        const subjectIdHeader = request.event.headers["Subject-Id"] || request.event['subject-id'];
+        console.log(request.event.headers);
+        const subjectIdHeader = request.event.headers["Subject-Id"] || request.event.headers['subject-id'];
         request.context.subjectId = subjectIdHeader
           ? new ObjectId(subjectIdHeader as string)
           : undefined;
         request.context.user = await getCurrentUser(request.event);
+        console.log(request.context);
       },
     })
     .use(
--- backend/src/functions/tests/countAssignmentsToGrade.ts
+++ backend/src/functions/tests/countAssignmentsToGrade.ts
@@ -9,7 +9,7 @@ const countAssignmentsToGrade = async (
 ) => {
   const db = await getDefaultDatabase();
   const attemptsCollection = db.collection("attempts");
-
+  console.log(JSON.stringify(context))
   const subjectId = context.subjectId;
 
   if (!subjectId) {
