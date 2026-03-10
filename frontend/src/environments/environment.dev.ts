ffc9ba8232da2660efedf6d811870a4b5a196aeb
Action merge <amatipierluigi@gmail.com>
2026-03-10 17:09:46 +0100

    fix

--- frontend/src/environments/environment.dev.ts
+++ frontend/src/environments/environment.dev.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: false,
 };
 
-export const apiUrl = 'http://localhost:3000';
+//export const apiUrl = 'http://localhost:3000';
+export const apiUrl =
+  'https://bkwshpcns1.execute-api.eu-south-1.amazonaws.com/prod';
