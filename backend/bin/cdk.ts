888ba7d8d79ffcfe87603656ccecce23b806d7b0
Action merge <amatipierluigi@gmail.com>
2026-03-31 18:15:29 +0200

    fixed region

--- backend/bin/cdk.ts
+++ backend/bin/cdk.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { CdkStack } from '../lib';
-import { STACK_NAME } from '../environment';
+import { AWS_REGION, STACK_NAME } from '../environment';
 
 const app = new cdk.App();
 new CdkStack(app, STACK_NAME, {
   env: {
-    region: 'eu-south-1'
+    region: AWS_REGION
   }
 });
