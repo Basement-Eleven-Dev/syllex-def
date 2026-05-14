84084edb7525ab7f675a14ecb971d69c520e1f96
Giulia <paonyts7@gmail.com>
2026-05-14 16:57:14 +0200

    feat: replace header and buttons in events page with Syllex components for consistency

--- frontend/src/teacher/pages/events/events.ts
+++ frontend/src/teacher/pages/events/events.ts
@@ -4,6 +4,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RouterModule } from '@angular/router';
 import { Calendario } from '../../components/calendario/calendario';
 import { TourAnchorNgBootstrapDirective } from 'ngx-ui-tour-ng-bootstrap';
+import { SyllexPageHeader } from '../../components/UI/syllex-page-header/syllex-page-header';
+import { SyllexButton } from '../../components/UI/syllex-button/syllex-button';
 
 @Component({
   selector: 'app-events',
@@ -12,6 +14,8 @@ import { TourAnchorNgBootstrapDirective } from 'ngx-ui-tour-ng-bootstrap';
     RouterModule,
     Calendario,
     TourAnchorNgBootstrapDirective,
+    SyllexPageHeader,
+    SyllexButton,
   ],
   templateUrl: './events.html',
   styleUrl: './events.scss',
